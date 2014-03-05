cbca5aaff21878de403a6d0002fa8abac52e1716
spinakr <anders.kfd@gmail.com>
2014-03-05 22:49:03 +0000

    Updated the popup prompt in dev page, and style some stuff

--- static/js/recap.js
+++ static/js/recap.js
@@ -13,7 +13,11 @@ $(function() {
     $(function() {
         $('#create_new_button').bind('click', function(e) {
             e.preventDefault();
-            $('#create_new_prompt').bPopup();
+            $('#create_new_prompt').bPopup({
+                easing: 'easeOutBack', //uses jQuery easing plugin
+                speed: 450,
+                transition: 'slideDown'
+            });
         });
     });
 })(jQuery);
