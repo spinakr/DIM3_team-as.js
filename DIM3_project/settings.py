51a0d8fb71cd2bc180430fbdd0c7197466e2a170
jaraunio <johannes.raunio@gmail.com>
2014-03-20 12:26:23 +0000

    Added pip requirements file.

--- DIM3_project/settings.py
+++ DIM3_project/settings.py
@@ -7,7 +7,7 @@ TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 STATIC_PATH = os.path.join(PROJECT_PATH,'static')
 DATABASE_PATH = os.path.join(PROJECT_PATH, 'recap.db')
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
